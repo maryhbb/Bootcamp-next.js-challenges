38e4c7d746b3b735486a278de709af31db62809d
Mary <76174247+maryhbb@users.noreply.github.com>
2023-09-05 12:25:33 +0200

    Implement react-styled-components/styled-navbar

--- react-styled-components_styled-navbar/pages/_app.js
+++ react-styled-components_styled-navbar/pages/_app.js
@@ -5,32 +5,71 @@ import styled from "styled-components";
 const Nav = styled.nav`
   border-bottom: solid 2px grey;
   padding: 1rem;
+  background-color: #fff;
+  width: 100%;
+  align-items: center;
 `;
 
 const StyledList = styled.ul`
   list-style: none;
   display: flex;
   gap: 1rem;
+  justify-content: space-around;
+  padding: 0;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #000;
+  font-weight: bold;
+  font-size: larger;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  animation: gradientAnimation 10s linear infinite alternate;
+  background: linear-gradient(45deg, #06b6d4, #f6e05e);
+  background-size: 400% 400%;
+`;
+
+const MainContent = styled.div`
+  background-color: #fff;
+  padding: 2rem;
+  margin-top: 20px;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+  width: 100%;
+  max-width: 800px;
+  text-align: center;
+  height: 30vh;
 `;
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <AppContainer>
       <GlobalStyle />
       <Nav>
         <StyledList>
           <li>
-            <Link href="/">Home</Link>
+            <StyledLink href="/">Home</StyledLink>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <StyledLink href="/about">About</StyledLink>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <StyledLink href="/contact">Contact</StyledLink>
           </li>
         </StyledList>
       </Nav>
-      <Component {...pageProps} />
-    </>
+      <MainContent>
+        <Component {...pageProps} />
+      </MainContent>
+    </AppContainer>
   );
 }
