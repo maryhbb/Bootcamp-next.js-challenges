f5bae174dcddff97430a4a8b16669ee9eae94b83
Mary <76174247+maryhbb@users.noreply.github.com>
2023-09-11 14:25:41 +0200

    Implement first part of task: Fetching with SWR

--- react-data-fetching_star-wars/pages/characters/1.js
+++ react-data-fetching_star-wars/pages/characters/1.js
@@ -1,25 +1,32 @@
+
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 import useSWR from "swr";
 
-
-
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default function Character(id) {
-
-  const { data, error, isLoading } = useSWR(` https://swapi.dev/api/people/${id}`, fetcher)
+export default function Character() {
   const id = 1;
+  const { data, error, isLoading } = useSWR(
+    ` https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
 
   return (
     <Layout>
-      <Card
-        id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
-      />
+      {data ? (
+        <Card
+          id={id}
+          name={data.name}
+          height={data.height}
+          eyeColor={data.eye_color}
+          birthYear={data.birth_year}
+        />
+      ) : (
+        <div>Loading...</div>
+      )}
     </Layout>
   );
 }
