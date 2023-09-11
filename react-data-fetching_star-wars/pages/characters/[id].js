5b33fb668e8dd054178f09389411d3f76d9a6eb3
Mary <76174247+maryhbb@users.noreply.github.com>
2023-09-11 15:32:57 +0200

    Implement react-data-fetching/star-wars task

--- react-data-fetching_star-wars/pages/characters/[id].js
+++ react-data-fetching_star-wars/pages/characters/[id].js
@@ -1,18 +1,30 @@
-
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 import useSWR from "swr";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
+const ErrorStyle = styled.div`
+  background-color: #ff6666;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 18px;
+  text-align: center;
+`;
+
 export default function Character() {
-  const id = 1;
+  const router = useRouter();
+  const { id } = router.query;
+
   const { data, error, isLoading } = useSWR(
-    ` https://swapi.dev/api/people/${id}`,
+    id ? `https://swapi.dev/api/people/${id}` : null,
     fetcher
   );
 
-  if (error) return <div>failed to load</div>;
+  if (error) return <ErrorStyle>Page not found</ErrorStyle>;
 
   return (
     <Layout>
